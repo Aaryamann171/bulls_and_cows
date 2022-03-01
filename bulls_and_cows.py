c9cdfd145e294577d5bf8df0a3b45a53c74c1d64
Aaryamann171 <aaryamann171@gmail.com>
2022-03-01 12:33:26 +0530

    major refactoring
    wrote this back in 2019, added to GH in 2020 before formatting my drive.
    Not really sure if it was even working before this refactoring job.

--- bulls_and_cows.py
+++ bulls_and_cows.py
@@ -1,69 +1,64 @@
 import random
 
-print("Welcome to the Bulls and Cow game!")
-
-def random_numbers_generator():
-    temp = []
-    for x in range(4):
-        temp.append(random.randint(0,9))
-    return temp
-
-def is_unique(l):
-    temp = []
-    for i in l:
-        if i not in temp:
-            temp.append(i)
-    temp_length = len(temp)
-    list_length = len(l)
-    if(temp_length<list_length):
-        return False
-    elif(temp_length==list_length):
-        return True
+def welcome_messages():
+    print("# Welcome to the Bulls and Cow game!")
+    print("Too lazy to type out rules, you can refer to the link below")
+    print("https://en.wikipedia.org/wiki/Bulls_and_Cows")
+    print("Let's Begin", end="\n\n")
 
 def secret_code_generator():
-    secret_code = random_numbers_generator()
-    if(is_unique(secret_code)):
-       # final_code = sum(d * 10**i for i, d in enumerate(secret_code[::-1]))
-        return(secret_code)
-    else:
-        return(secret_code_generator())
+    secret_code = []
+    num_list = [x for x in range(1,10)]
+    for i in range(4):
+        rand_indx = random.randint(1, len(num_list)-1)
+        x = num_list.pop(rand_indx)
+        secret_code.append(x)
+    original_num = int("".join([str(x) for x in secret_code]))
+    return secret_code, original_num
 
 def user_input():
+    # quit if user types one of these commands
+    quit_messages = ["quit", "exit", "q", "ex"]
     guess = input("Enter you guess: ")
-    guess_list = []
-    while(guess>0):
-        digit = guess%10
-        guess_list.append(digit)
-        guess=guess/10
-    return guess_list[::-1]
-    
-def bulls(l1,l2): #correct in correct position
+    if guess in quit_messages:
+        print("Quiting game, thanks for playing!")
+        exit()
+    if len(guess) != 4:
+        print("Guess must be 4 digits")
+        return None
+    # convert user guess into a list of ints
+    guess_list = list(map(lambda i: int(i), [c for c in guess]))
+    # check if there are any duplicates in the list
+    if len(set(guess_list)) < 4:
+        print("NOTE: all numbers are unique")
+    return guess_list
+
+def bulls(l1, l2): # correct guess in the correct position
     bulls = 0
-    for i in range(len(l1)):
-        if(l1[i]==l2[i]):
-            bulls=bulls+1
+    for i in range(4):
+        if l1[i] == l2[i]:
+            bulls += 1
     return bulls
 
-def cows_temp(l1, l2): #correct but may not be in correct place
-    cows = 0
-    for i in l1:
-        if i in l2:
-            cows=cows+1
-    return cows
+def common_nums(l1, l2): # correct guess but not in the correct position
+    return len(set(l1).intersection(set(l2)))
 
 def main():
-    flag = 1
-    x = secret_code_generator()
-    while(flag): 
-        y = user_input()
-        b = bulls(x,y)
-        c = cows_temp(x,y)
-        c_final = c - b
-        print("Bulls: "+str(b))
-        print("Cows: "+str(c_final))
-        if(x==y):
-            print("Congratulations! Your guess is correct.")
-            flag = 0
+    welcome_messages()
+    game_on_flag = 1
+    secret_code, original_num = secret_code_generator()
+    while(game_on_flag): 
+        user_guess = user_input()
+        while (user_guess is None):
+            user_guess = user_input()
+        n_bulls = bulls(secret_code, user_guess)
+        commons = common_nums(secret_code, user_guess)
+        n_cows = commons - n_bulls
+        print(f"Bulls: {n_bulls}")
+        print(f"Cows: {n_cows}")
+        if(user_guess == secret_code):
+            print(f"Congratulations! Your guess is correct. Secret code was in fact {original_num}.")
+            game_on_flag = 0
         
 if __name__=="__main__":
     main()
--- readme.md
+++ readme.md
@@ -1,2 +1,6 @@
 # Bulls & Cows
 Python implementation of the infamous game with the same name.
+
+### TODOS
+- [ ] Add type hinting
+- [ ] Add unit tests (?)
